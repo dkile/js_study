b2c306c06c76ceb78167487fd147fd7c1890f2e8
dkile <dkile532@naver.com>
2021-07-24 20:56:46 +0900

    js_study: scroll paging1

--- pagingExample/paging.js
+++ pagingExample/paging.js
@@ -2,29 +2,35 @@ window.onload = function () {
   const bgColorArr = ["#85FFBD", "#FFFB7D", "#E0C3FC", "#00DBDE"];
   const prevBtn = document.getElementById("prev");
   const nextBtn = document.getElementById("next");
+  const section = document.getElementsByTagName("section");
 
   const totalPage = bgColorArr.length;
   let pageNum = Math.round(Math.random() * (totalPage - 1));
   let timer;
 
-  function bgColorChange() {
+  function pageChange() {
     document.getElementsByTagName("body")[0].style.background =
       bgColorArr[pageNum];
+
+    window.scrollTo({
+      top: section[pageNum].offsetTop,
+      behavior: "smooth",
+    });
   }
 
   prevBtn.addEventListener("click", function () {
     if (pageNum > 0) pageNum--;
     else pageNum = totalPage - 1;
-    bgColorChange();
+    pageChange();
   });
   nextBtn.addEventListener("click", function () {
     if (pageNum < totalPage - 1) pageNum++;
     else pageNum = 0;
-    bgColorChange();
+    pageChange();
   });
-  timer = setInterval(function () {
+  /*timer = setInterval(function () {
     if (pageNum > 0) pageNum--;
     else pageNum = totalPage - 1;
     bgColorChange();
-  }, 1000);
+  }, 1000);*/
 };
