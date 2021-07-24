b3ecd8dc6f03b0e04b44bf4553b4558001756a1c
dkile <dkile532@naver.com>
2021-07-24 21:33:22 +0900

    js_study: scroll paging2

--- pagingExample/paging.js
+++ pagingExample/paging.js
@@ -4,33 +4,54 @@ window.onload = function () {
   const nextBtn = document.getElementById("next");
   const section = document.getElementsByTagName("section");
 
-  const totalPage = bgColorArr.length;
+  const totalPage = section.length;
   let pageNum = Math.round(Math.random() * (totalPage - 1));
   let timer;
 
   function pageChange() {
     document.getElementsByTagName("body")[0].style.background =
       bgColorArr[pageNum];
-
-    window.scrollTo({
-      top: section[pageNum].offsetTop,
-      behavior: "smooth",
-    });
   }
 
   prevBtn.addEventListener("click", function () {
     if (pageNum > 0) pageNum--;
     else pageNum = totalPage - 1;
     pageChange();
+    window.scrollTo({
+      top: section[pageNum].offsetTop,
+      behavior: "smooth",
+    });
   });
   nextBtn.addEventListener("click", function () {
     if (pageNum < totalPage - 1) pageNum++;
     else pageNum = 0;
     pageChange();
+    window.scrollTo({
+      top: section[pageNum].offsetTop,
+      behavior: "smooth",
+    });
   });
   /*timer = setInterval(function () {
     if (pageNum > 0) pageNum--;
     else pageNum = totalPage - 1;
     bgColorChange();
   }, 1000);*/
+  window.addEventListener("scroll", function (event) {
+    let scroll = this.scrollY;
+
+    for (let i = 0; i < totalPage; i++) {
+      if (
+        scroll > section[i].offsetTop - window.outerHeight / 2 &&
+        scroll <
+          section[i].offsetTop -
+            window.outerHeight / 2 +
+            section[i].offsetHeight
+      ) {
+        pageNum = i;
+        break;
+      }
+    }
+    pageChange();
+  });
+  pageChange();
 };
