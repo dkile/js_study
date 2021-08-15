21eef07cd4c5367dd6df22e1902939ae7d2644ce
dkile <dkile532@naver.com>
2021-08-16 00:49:21 +0900

    js_study: make reduce  handle Promise

--- functionalP/latency/latency.js
+++ functionalP/latency/latency.js
@@ -3,32 +3,24 @@ const curry =
   (a, ..._) =>
     _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-const map = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) {
-    res.push(f(a));
-  }
-  return res;
-});
-
-const filter = curry((f, iter) => {
-  let res = [];
-  for (const a of iter) {
-    if (f(a)) res.push(a);
-  }
-  return res;
-});
-
 const go1 = (a, f) => (a instanceof Promise ? a.then(f) : f(a));
 
+const reduceF = (acc, a, f) =>
+  a instanceof Promise
+    ? a.then(
+        (a) => f(acc, a),
+        (e) => (e == nop ? acc : Promise.reject(e))
+      )
+    : f(acc, a);
+
+const head = (iter) => go1(take(1, iter), ([h]) => h);
+
 const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  }
+  if (!iter) acc = head((iter = acc[Symbol.iterator]()));
   return go1(acc, function recur(acc) {
-    for (const a of iter) {
-      acc = f(acc, a);
+    let cur;
+    while (!(cur = iter.next()).done) {
+      acc = reduceF(acc, cur.value, f);
       if (acc instanceof Promise) {
         return acc.then(recur);
       }
@@ -63,102 +55,96 @@ L.range = function* (l) {
   }
 };
 
-/**
- * Test
- */
-// const list = range(10000000);
-// console.log(reduce(add, list));
-
-// const l_list = L.range(10000000);
-// console.log(reduce(add, l_list));
-
-/**
- * L.map
- */
-// L.map = curry(function* (f, iter) {
-//   iter = iter[Symbol.iterator]();
-//   let cur;
-//   while (!(cur = iter.next()).done) {
-//     const a = cur.value;
-//     yield f(a);
-//   }
-// });
 L.map = curry(function* (f, iter) {
   for (const a of iter) {
     yield go1(a, f);
   }
 });
 
+const nop = Symbol("nop");
+
 L.filter = curry(function* (f, iter) {
   for (const a of iter) {
-    if (f(a)) yield a;
+    const b = go1(a, f);
+    if (b instanceof Promise)
+      yield b.then((b) => (b ? a : Promise.reject(nop)));
+    else if (b) yield a;
   }
 });
 
 const take = curry((l, iter) => {
   let res = [];
-  return (function recur() {
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
+  // return (function recur() {
+  //   var _iteratorNormalCompletion = true;
+  //   var _didIteratorError = false;
+  //   var _iteratorError = undefined;
 
-    try {
-      for (
-        var _iterator = iter[Symbol.iterator](), _step;
-        !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
-        _iteratorNormalCompletion = true
-      ) {
-        var _a = _step.value;
-        if (_a instanceof Promise)
-          return _a.then((_a) => {
-            res.push(_a);
+  //   try {
+  //     for (
+  //       var _iterator = iter[Symbol.iterator](), _step;
+  //       !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
+  //       _iteratorNormalCompletion = true
+  //     ) {
+  //       var _a = _step.value;
+  //       if (_a instanceof Promise)
+  //         return _a.then((_a) => {
+  //           res.push(_a);
+  //           return res.length === l ? res : recur();
+  //         });
+  //       res.push(_a);
+  //       if (res.length === l) return res;
+  //     }
+  //   } catch (err) {
+  //     _didIteratorError = true;
+  //     _iteratorError = err;
+  //   } finally {
+  //     try {
+  //       if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+  //         _iterator["return"]();
+  //       }
+  //     } finally {
+  //       if (_didIteratorError) {
+  //         throw _iteratorError;
+  //       }
+  //     }
+  //   }
+  //   return res;
+
+  //   for (const a of iter) {
+  //     if (a instanceof Promise)
+  //       return a.then((a) => {
+  //         res.push(a);
+  //         return res.length === l ? res : recur();
+  //       });
+  //     res.push(a);
+  //     if (res.length === l) return res;
+  //   }
+  //   return res;
+  return (function recur() {
+    let cur;
+    while (!(cur = iter.next()).done) {
+      let a = cur.value;
+      if (a instanceof Promise)
+        return a
+          .then((a) => {
+            res.push(a);
             return res.length === l ? res : recur();
-          });
-        res.push(_a);
-        if (res.length === l) return res;
-      }
-    } catch (err) {
-      _didIteratorError = true;
-      _iteratorError = err;
-    } finally {
-      try {
-        if (!_iteratorNormalCompletion && _iterator["return"] != null) {
-          _iterator["return"]();
-        }
-      } finally {
-        if (_didIteratorError) {
-          throw _iteratorError;
-        }
-      }
+          })
+          .catch((e) => (e == nop ? recur() : Promise.reject(e)));
+      res.push(a);
+      if (res.length === l) return res;
     }
     return res;
-    //   for (const a of iter) {
-    //     if (a instanceof Promise)
-    //       return a.then((a) => {
-    //         res.push(a);
-    //         return res.length === l ? res : recur();
-    //       });
-    //     res.push(a);
-    //     if (res.length === l) return res;
-    //   }
-    //   return res;
-    // })();
-    // return (function recur() {
-    //   let cur;
-    //   while (!(cur = iter.next()).done) {
-    //     let a = cur.value;
-    //     if (a instanceof Promise)
-    //       return a.then((a) => {
-    //         res.push(a);
-    //         return res.length === l ? res : recur();
-    //       });
-    //     res.push(a);
-    //     if (res.length === l) return res;
-    //   }
-    //   return res;
-    // })();
   })();
 });
+
+const takeAll = take(Infinity);
+
+const map = curry(pipe(L.map, takeAll));
+
+const filter = curry(pipe(L.filter, takeAll));
+
+const find = curry((f, iter) => go(iter, L.filter(f), take(1), ([a]) => a));
 
 const join = curry((sep = ",", iter) =>
   reduce((a, b) => `${a}${sep}${b}`, iter)
@@ -174,7 +160,6 @@ const queryStr = pipe(
   join("&")
 );
 //console.log(queryStr({ limit: "10", offset: "10", type: "notice" }));
-const takeAll = take(Infinity);
 
 const isIterable = (a) => a && a[Symbol.iterator];
 //yield* iterable == for (const val of iterable) yield val
@@ -199,18 +184,10 @@ L.flatMap = curry(pipe(L.map, L.flatten));
 
 const flatMap = curry(pipe(L.flatMap, flatten));
 
-// go(
-//   1,
-//   (a) => a + 10,
-//   (a) => Promise.resolve(a + 100),
-//   (a) => a + 1000,
-//   (a) => a + 10000,
-//   console.log
-// );
-
 go(
-  [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)],
-  L.map((a) => a + 10),
-  take(2),
+  [1, 2, 3, 4],
+  L.map((a) => Promise.resolve(a * a)),
+  L.filter((a) => Promise.resolve(a % 2)),
+  reduce(add),
   console.log
 );
