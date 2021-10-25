0a152e12a1432e24fe35ce9e80888e4abcf745bf
dkile <dkile532@naver.com>
2021-10-25 20:48:37 +0900

    complete querystring, crypto, util, worker_threads module practice

--- node/03/require/require.js
+++ node/03/require/require.js
@@ -98,3 +98,110 @@ const nodeURL = url.parse(
 );
 console.log("url.parse():", nodeURL);
 console.log(url.format(nodeURL));
+
+// querystring
+const querystring = require("querystring");
+
+const nodeURL2 = url.parse(
+  "http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000&limit=10&page=3&category=nodejs&category=javascript#anchor"
+);
+const query = querystring.parse(nodeURL2.query);
+console.log("querystring.parse():", query);
+console.log("querystring.stringify():", querystring.stringify(query));
+
+// crypto
+const crypto = require("crypto");
+
+console.log("crypto------------------------------------------");
+console.log(
+  "base64:",
+  crypto.createHash("sha512").update("password").digest("base64")
+);
+console.log(
+  "hex:",
+  crypto.createHash("sha512").update("password").digest("hex")
+);
+console.log(
+  "base64:",
+  crypto.createHash("sha512").update("another password").digest("base64")
+);
+
+// crypto_pbkdf2
+crypto.randomBytes(64, (err, buf) => {
+  const salt = buf.toString("base64");
+  console.log("salt:", salt);
+  crypto.pbkdf2("password", salt, 100000, 64, "sha512", (err, key) => {
+    console.log("password:", key.toString("base64"));
+  });
+});
+
+// crypto_cipheriv
+const cipher_algorithm = "aes-256-cbc";
+const key = "abcdefghijklmnopqrstuvwxyz123456";
+const iv = "1234567890123456";
+
+const cipher = crypto.createCipheriv(cipher_algorithm, key, iv);
+let encrypted = cipher.update("암호화", "utf-8", "base64");
+encrypted += cipher.final("base64");
+console.log("암호화:", encrypted);
+
+const decipher = crypto.createDecipheriv(cipher_algorithm, key, iv);
+let decrypted = decipher.update(encrypted, "base64", "utf-8");
+decrypted += decipher.final("utf-8");
+console.log("복호화:", decrypted);
+
+// util
+const util = require("util");
+
+const dontUseMe = util.deprecate((x, y) => {
+  console.log(x + y);
+}, "dontUseMe function deprecated!!");
+dontUseMe(1, 2);
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+randomBytesPromise(64)
+  .then((buf) => {
+    console.log(buf.toString("base64"));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// worker_threads
+const {
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData,
+} = require("worker_threads");
+
+if (isMainThread) {
+  const threads = new Set();
+  threads.add(
+    new Worker(__filename, {
+      workerData: { start: 1 },
+    })
+  );
+  threads.add(
+    new Worker(__filename, {
+      workerData: { start: 2 },
+    })
+  );
+  for (const worker of threads) {
+    worker.on("message", (message) => console.log("from worker ", message));
+    worker.on("exit", () => {
+      threads.delete(worker);
+      if (threads.size === 0) {
+        console.log("job end");
+      }
+    });
+  }
+} else {
+  const data = workerData;
+  parentPort.postMessage(data.start + 100);
+  // parentPort.on('message', v => {
+  //   console.log('from parent ', v);
+  //   parentPort.postMessage('pong');
+  //   parentPort.close();
+  // });
+}
