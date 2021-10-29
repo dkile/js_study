e6e29c0506e71f65ac82e2a27e02186029eab091
dkile <dkile532@naver.com>
2021-10-29 14:37:08 +0900

    complete fs module practice

--- node/03/module_practice/readFile.js
+++ node/03/module_practice/readFile.js
@@ -12,41 +12,82 @@ const fs = require("fs");
 // promise 형태
 const fs2 = require("fs").promises;
 
-fs2
-  .readFile("./readme.txt")
-  .then((data) => {
-    console.log(data);
-    console.log(data.toString());
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// fs2
+//   .readFile("./readme.txt")
+//   .then((data) => {
+//     console.log(data);
+//     console.log(data.toString());
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-fs2
-  .writeFile("./writeme.txt", "글이 입력됩니다.")
-  .then(() => fs2.readFile("./writeme.txt"))
-  .then((data) => {
-    console.log(data.toString());
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// fs2
+//   .writeFile("./writeme.txt", "글이 입력됩니다.")
+//   .then(() => fs2.readFile("./writeme.txt"))
+//   .then((data) => {
+//     console.log(data.toString());
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // stream 형식
-const readStream = fs.createReadStream("./readme.txt", { highWaterMark: 16 });
-const data = [];
+// const readStream = fs.createReadStream("./readme.txt", { highWaterMark: 16 });
+// const data = [];
 
-readStream.on("data", (chunk) => {
-  data.push(chunk);
-  console.log("data: ", chunk, chunk.length);
-});
-readStream.on("end", () => {
-  console.log("end: ", Buffer.concat(data).toString());
-});
-readStream.on("error", (err) => {
-  console.log("error: ", err);
-});
+// readStream.on("data", (chunk) => {
+//   data.push(chunk);
+//   console.log("data: ", chunk, chunk.length);
+// });
+// readStream.on("end", () => {
+//   console.log("end: ", Buffer.concat(data).toString());
+// });
+// readStream.on("error", (err) => {
+//   console.log("error: ", err);
+// });
 
-// stream pipe
-const writeStream = fs.createWriteStream("writeme2.txt");
-readStream.pipe(writeStream);
+// // stream pipe
+// const writeStream = fs.createWriteStream("writeme2.txt");
+// readStream.pipe(writeStream);
+
+// // create file
+// const status = require("fs").constants;
+
+// fs2
+//   .access("./createExample", status.F_OK | status.W_OK | status.R_OK)
+//   .then(() => Promise.reject("이미 폴더 있음"))
+//   .catch((err) =>
+//     err.code == "ENOENT" ? fs2.mkdir("./createExample") : Promise.reject(err)
+//   )
+//   .then(() => fs2.open("./createExample/createExample.js", "w"))
+//   .then(() =>
+//     fs2.rename("./createExample/createExample.js", "./createExample/example.js")
+//   )
+//   .then(() => console.log("파일 이름 바꾸기 성공"))
+//   .catch((err) => console.log(err));
+
+// delete folder
+// fs2
+//   .readdir("./createExample")
+//   .then((dir) => {
+//     console.log("폴더: ", dir);
+//     return fs2.unlink("./createExample/example.js");
+//   })
+//   .then(() => fs2.rmdir("./createExample"))
+//   .catch((err) => console.log(err));
+
+// copy file
+fs2
+  .copyFile("writeme.txt", "writeme3.txt")
+  .then(() => {
+    console.log("복사 완료");
+    return fs2.readFile("./writeme.txt");
+  })
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err));
+
+// watch
+fs.watch("./writeme.txt", (eventType, filename) => {
+  console.log(eventType, filename);
+});
