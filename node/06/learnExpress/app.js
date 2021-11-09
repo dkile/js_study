dd2ca47f16bf23c4cca88598d305769f9f4750ed
dkile <dkile532@naver.com>
2021-11-10 03:40:33 +0900

    complete middleware practice

--- node/06/learnExpress/app.js
+++ node/06/learnExpress/app.js
@@ -1,11 +1,83 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const multer = require("multer");
+const fs = require("fs");
+const dotenv = require("dotenv");
 const path = require("path");
+
+dotenv.config();
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
+
+try {
+  fs.readdirSync("uploads");
+} catch (err) {
+  console.error("make uploads folder");
+  fs.mkdirSync("uploads");
+}
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send("ok");
+  }
+);
+app.use((req, res, next) => {
+  console.log("모든 요청에 다 실행됩니다.");
+  next();
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+    next();
+  },
+  (req, res) => {
+    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get("port"), () => {
