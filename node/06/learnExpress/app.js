6401775c54087ca58caa29b1819a3ea4855378f3
dkile <dkile532@naver.com>
2021-11-10 15:33:05 +0900

    complete router practice

--- node/06/learnExpress/app.js
+++ node/06/learnExpress/app.js
@@ -8,9 +8,13 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config();
+const indexRouter = require("./routes");
+const userRouter = require("./routes/user");
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -29,6 +33,13 @@ app.use(
     name: "session-cookie",
   })
 );
+
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
+app.use((req, res, next) => {
+  res.status(404).send("Not Found");
+});
 
 try {
   fs.readdirSync("uploads");
