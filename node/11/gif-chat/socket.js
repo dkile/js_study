b2e2f42384522c717838645809a433519293cfc9
dkile <dkile532@naver.com>
2021-11-29 22:26:16 +0900

    complete socket.io practice

--- node/11/gif-chat/socket.js
+++ node/11/gif-chat/socket.js
@@ -1,26 +1,47 @@
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 
 module.exports = (server) => {
-  const wss = new WebSocket.Server({ server });
+  // const wss = new WebSocket.Server({ server });
+  const io = SocketIO(server, { path: "/socket.io" });
 
-  wss.on("connection", (ws, req) => {
+  io.on("connection", (socket) => {
+    const req = socket.request;
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("new client accessed", ip);
-    ws.on("message", (message) => {
-      console.log(message.toString());
+    console.log("new client accessed", ip, socket.id, req.ip);
+    socket.on("disconnect", () => {
+      console.log("client connection end", ip, socket.id);
+      clearInterval(socket.interval);
     });
-    ws.on("error", (err) => {
+    socket.on("error", (err) => {
       console.error(err);
     });
-    ws.on("close", () => {
-      console.log("client connection end", ip);
-      clearInterval(ws.interval);
+    socket.on("reply", (data) => {
+      console.log(data);
     });
-
-    ws.interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("message from server to client");
-      }
+    socket.interval = setInterval(() => {
+      socket.emit("news", "Hello Socket.IO");
     }, 3000);
   });
+
+  //   wss.on("connection", (ws, req) => {
+  //     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  //     console.log("new client accessed", ip);
+  //     ws.on("message", (message) => {
+  //       console.log(message.toString());
+  //     });
+  //     ws.on("error", (err) => {
+  //       console.error(err);
+  //     });
+  //     ws.on("close", () => {
+  //       console.log("client connection end", ip);
+  //       clearInterval(ws.interval);
+  //     });
+
+  //     ws.interval = setInterval(() => {
+  //       if (ws.readyState === ws.OPEN) {
+  //         ws.send("message from server to client");
+  //       }
+  //     }, 3000);
+  //   });
 };
